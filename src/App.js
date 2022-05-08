79adc4e496968696fa60c35414f999be808f2754
saket105 <saketcool35@gmail.com>
2022-05-08 21:43:16 +0530

    Nav section done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Login from "./component/Login/login";
 import Register from "./component/Register/Register";
 import { Route, Routes } from "react-router-dom";
 import NavBar from "./component/NavBar/Navbar";
+import Home from "./component/Home/Home";
 
 function App() {
   return (
     <div>
+      <NavBar />
       <Routes>
-        <NavBar />
+        <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
       </Routes>
